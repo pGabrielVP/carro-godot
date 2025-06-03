88ec8637ee913ff9638e60da7575fa2d9ae66663
pGabrielVP <paulogabrielprado@hotmail.com>
2025-06-03 13:02:26 -0300

    Add files via upload
    reduzido diâmetro das rodas, variáveis criadas, nova equação para definir engine_force, marcha tem um efeito na aceleração do carro

--- exemplo/vehicle_body_3d.gd
+++ exemplo/vehicle_body_3d.gd
@@ -1,11 +1,15 @@
 extends VehicleBody3D
 
-enum marcha {neutro=0, primera=10, segunda=30, terceira=60, quarta=80, quinta=150, re=10}
+var torque: int = 500
+var drive: float = 4.20
+var gears: Array[float] = [3.30, 2.20, 1.80, 1.50, 1.30]
+var current_gear = 0
+const wheel_radius: float = 0.255
 
 const STEER_SPEED:float  = 1.5
 const STEER_LIMIT:float = 0.4
 var steer_target:float = 0
-var aceleracao:int = 200
+
 var acelerador: float = 0
 
 func _physics_process(delta: float) -> void:
@@ -15,19 +19,16 @@ func _physics_process(delta: float) -> void:
 	steer_target *= STEER_LIMIT
 
 	if Input.is_action_pressed("acelerador_leve"):
-		acelerador += 10
+		acelerador += 0.1
 	if Input.is_action_pressed("acelerador_medio"):
-		acelerador += 20
+		acelerador += 0.2
 	if Input.is_action_pressed("acelerador_pesado"):
-		acelerador += 40
-
+		acelerador += 0.4
+	
 	if acelerador > 0:
-		var speed = linear_velocity.length()
-		if speed < acelerador:
-			engine_force = aceleracao
-		else: 
-			engine_force = 0
+		acelerador += 0.3 # 0.4, 0.5, 0.7 ;; sem essa linha é impossivel chegar a 1.0
+		engine_force = (torque * acelerador * (gears[current_gear] * drive)) / wheel_radius
 	else:
 		engine_force = 0
-
+	print(linear_velocity)
 	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
