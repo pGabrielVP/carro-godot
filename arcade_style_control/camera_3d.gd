4e0cc5cc6cbe4549cc49aaaa3349d8c9ccda0e1e
pGabrielVP <paulogabrielprado@hotmail.com>
2025-06-06 22:10:38 -0300

    camera
    camera

--- arcade_style_control/camera_3d.gd
+++ arcade_style_control/camera_3d.gd
@@ -1,8 +1,9 @@
 extends Camera3D
 
 @onready var carro: Node3D = $"../suv"
+@onready var bola: RigidBody3D = $"../RigidBody3D"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = lerp(position, carro.position + Vector3(0.0, 2.27, -3.568), 10 * delta)
-	
+	global_position = bola.global_position + Vector3(8,4,0)
+	global_transform.basis.x = -carro.global_transform.basis.x
