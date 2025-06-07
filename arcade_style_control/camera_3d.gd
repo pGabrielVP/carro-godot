36960d2a42c867dc9faf5ab8de45be358346a6ed
pGabrielVP <paulogabrielprado@hotmail.com>
2025-06-06 22:29:49 -0300

    arruma camera

--- arcade_style_control/camera_3d.gd
+++ arcade_style_control/camera_3d.gd
@@ -4,6 +4,9 @@ extends Camera3D
 @onready var bola: RigidBody3D = $"../RigidBody3D"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	global_position = bola.global_position + Vector3(8,4,0)
-	global_transform.basis.x = -carro.global_transform.basis.x
+func _process(_delta: float) -> void:
+	#position = lerp(position, carro.position + Vector3(0.0, 2.27, -3.568), 10 * delta)
+
+	#global_position = bola.global_position + Vector3(8,4,0)
+	#global_transform.basis.x = -carro.global_transform.basis.x
+	pass
